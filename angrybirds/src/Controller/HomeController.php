6a979144708d07de2b15b036ffe5e78a9bedeb65
Taikhun <taiyang78190@gmail.com>
2021-11-09 16:45:26 +0100

    creation of homecontroller via route.yaml

---
+++ angrybirds/src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php 
+namespace App;
+
+class HomeController{
+ /**
+  * This method displays the homepage, list of all birds
+  * @return void
+  *
+  */
+  public function home()
+  {
+      dd('Hello World!');
+  }
+}
