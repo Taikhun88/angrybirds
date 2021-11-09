f2c7fac2a1dc4c9d51df11c2e1ec6ba2e8629077
Taikhun <taiyang78190@gmail.com>
2021-11-09 17:14:20 +0100

    feature create home controller with routes.yaml -DONE

--- angrybirds/src/Controller/HomeController.php
+++ angrybirds/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class HomeController{
  /**
   * This method displays the homepage, list of all birds
@@ -10,6 +12,9 @@ class HomeController{
   */
   public function home()
   {
-      dd('Hello World!');
+      //dd('Hello World!');
+      // this method has its route name and url defined in file routes.yaml
+      // initially without injection of Response, symfony can only return object. 
+      return new Response('<h1>Hello Angry Birds!</h1>');
   }
 }
