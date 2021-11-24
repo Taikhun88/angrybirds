f81f942bad9f123855fc1f5a29f5855f885fc621
Taikhun <taiyang78190@gmail.com>
2021-11-24 12:50:22 +0100

    feature DEBUG asset loop twig bird --DOING

--- angrybirds/src/Controller/HomeController.php
+++ angrybirds/src/Controller/HomeController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Model\Birds;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController{
- /**
-  * This method displays the homepage, list of all birds
-  *
-  * @return void
-  */
-  public function home()
+
+class HomeController extends AbstractController{
+  /**
+   * Displays the homepage with list of all birds
+   * This methods displays the route thanks to annotations.yaml and uses injection of Route above
+   * 
+   * @Route("/")
+   * 
+   * return Response
+   */
+  public function home(): Response
   {
       //dd('Hello World!');
       // this method has its route name and url defined in file routes.yaml
       // initially without injection of Response, symfony can only return object. 
-      return new Response('<h1>Hello Angry Birds!</h1>');
+      $birdsModel = new Birds();
+      $birds = $birdsModel->getBirds();
+      return $this->render('home/home.html.twig', [
+        'title' => "Hello Angry Birds",
+        'birds' => $birds
+      ]);
   }
 }
